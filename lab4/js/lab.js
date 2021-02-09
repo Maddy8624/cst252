10f92868caffada5aef5cf156cbb55ba9b15fa3b
Maddy8624 <mwilley@csumb.edu>
2021-02-08 17:12:19 -0800

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,17 @@
+//hangman.js
+//A Hangman game against the computer
+//
+//Author: Maddy Willey <mwilley@csumb.edu>
+//Date: 2021
+
+
+
+// Making a Hangman Game.
+    // Choose a word.
+    // Create the hangman board.
+    // Accept user input.
+    // Evaluate if the user chose the letter correctly.
+        // Place the letter on screen if part of the chosen word.
+        // Draw part of the hangman if the letter chosen is not part of the word.
+    // Test for Win! Condition
+//
