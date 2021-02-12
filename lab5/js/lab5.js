a7a41099965f9cc7ed6b5882100f9e691ed5fac1
Maddy8624 <mwilley@csumb.edu>
2021-02-11 18:01:28 -0800

    lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,31 @@
+//Lab 5: Working with Variables
+//Author: Maddy Willey <mwilley@csumb.edu>
+//Created: Feb 11, 2021
+//License: Public Domain
+//Javavscript code borrowed from We Modes <wmodes@csumb.edu>
+
+
+console.log("Here we go")
+
+
+//Define Variables
+make = "Honda";
+model = "Fit";
+color = "Blue";
+year = 2007;
+
+
+//Declare a boolean Variables
+var ownIt = true;
+
+
+//calculate
+var age = new Date().getFullYear();
+
+
+//output
+document.writeIN("Make: " + make + "<br>");
+document.writeIN("Model: " + model + "<br>");
+document.writeIN("Color: " + color + "<br>");
+document.writeIN("Year: " + year + "<br>");
+document.writeIN("Age : " + age + "<br>");
