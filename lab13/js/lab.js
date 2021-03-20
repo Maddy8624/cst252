c21efdb104fe995b1f1225d1603adbc3423cafc9
Maddy8624 <mwilley@csumb.edu>
2021-03-20 10:38:07 -0700

    Lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,6 @@
+//Lab 12: Loops
+//Author: Maddy Willey <mwilley@csumb.edu>
+//Created: March 11, 2021
+//License: Public Domain
+//Javavscript code borrowed from Wes Modes <wmodes@csumb.edu>
+//I would also like to note that the majority of this lab was based on Wes's already existing HTML and Javascript
