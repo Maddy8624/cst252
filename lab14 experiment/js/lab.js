872a208f5cc94cfcb91f8d507d1191ea26ff802c
Maddy8624 <mwilley@csumb.edu>
2021-03-28 14:02:49 -0700

    lab 15

---
+++ lab14 experiment/js/lab.js
@@ -0,0 +1,35 @@
+//Lab 14: Javascript Classes & Objects experiment
+
+//Author: Maddy Willey <mwilley@csumb.edu>
+//Created: March 23, 2021
+//License: Public Domain
+//Javavscript code borrowed from Wes Modes <wmodes@csumb.edu>
+//I would also like to note that the majority of this lab was based on Wes's already existing HTML and Javascript
+
+
+//This is the NEW class method
+class Vehicle {
+  constructor(make, model, year, color, extras, name) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.extras = extras;
+    this.name = name;
+  }
+  info (){
+      return this.color + " " + this.year + " " + this.make + " " +  this.model + " with " +  this.extras + " called " + this.name;
+    }
+  }
+
+var vehicles = [];
+
+vehicles.push(new Vehicle("Toyota", "Prius", 2015, "Red", "License plate is the name of the car", "ladybug"));
+vehicles.push(new Vehicle("Ford", "Mustang", 2005, "Blue", "Yellow Racing Stripe", "Speedy"));
+
+outputEl = document.getElementById("output");
+for (var i = 0; i<vehicles.length; i++) {
+  var newEl = document.createElement("li");
+  newEl.innerText = vehicles[i].info();
+  outputEl.appendChild(newEl);
+}
